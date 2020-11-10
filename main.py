f797fc09de3811f1e84316891a104880126df2fc
yuki1201” <yuki12017825@gmail.com>
2020-11-10 21:33:43 +0900

    renew design

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def calc():
 
 win = tk.Tk()
 win.title("万能でんたくんα")
-win.geometry("320x270")
+win.geometry("300x250")
 win.configure(background='#3E4149')
 disp = tk.Entry(win, font=('', 20), justify="center",highlightbackground='#3E4149')
 disp.pack(fill='x')
@@ -85,11 +85,11 @@ disp.bind('<Return>', calc)
 label = tk.Label(win, font=('', 20), anchor="center",highlightbackground='#3E4149')
 label.pack(fill='x')
 fr = tk.Frame(win)
-fr.pack()
+fr.pack(expand = True)
 for y, cols in enumerate(BUTTONS):
     for x, n in enumerate(cols):
-      btn = tk.Button(fr,text=n,font=('', 20),width=3,height=1,highlightbackground='#3E4149')
-      btn.grid(row=y+1, column=x+1)
+      btn = tk.Button(fr,text=n,font=('', 20),width=0,height=0,highlightbackground='#3E4149')
+      btn.grid(row=y+1, column=x+1, sticky = 'nsew')
       btn.bind('<1>', make_click(n))
 
 win.mainloop()
